13bd4dcbe7b1ff4d045ea6bd04948e1a2fa735df
Jeremy Kahn <jeremyckahn@gmail.com>
2018-02-04 11:08:44 -0600

    Wire up toolDragStartX/toolDragStartY to drag event handlers

--- src/components/merkaba.event-handlers.js
+++ src/components/merkaba.event-handlers.js
@@ -12,14 +12,25 @@ export default {
    * @param {external:React.SyntheticEvent} e
    * @param {external:Draggable.DraggableData} data
    */
-  handleCanvasDragStart (e, data) {
-    this.setState({ isDraggingTool: true });
+  handleCanvasDragStart (e, { x: toolDragStartX, y: toolDragStartY }) {
+    this.setState({
+      isDraggingTool: true,
+      toolDragStartX,
+      toolDragStartY
+    });
   },
 
   handleCanvasDrag () {
   },
 
+  /**
+   * @method merkaba.Merkaba#handleCanvasDragStop
+   */
   handleCanvasDragStop () {
-    this.setState({ isDraggingTool: false });
+    this.setState({
+      isDraggingTool: false,
+      toolDragStartX: null,
+      toolDragStartY: null
+    });
   },
 };
--- test/components/merkaba.event-handlers.js
+++ test/components/merkaba.event-handlers.js
@@ -23,11 +23,19 @@ describe('eventHandlers', () => {
 
   describe('Merkaba#handleCanvasDragStart', () => {
     beforeEach(() => {
-      component.instance().handleCanvasDragStart();
+      component.instance().handleCanvasDragStart(null, { x: 10, y: 15 });
     });
 
     it('sets the isDraggingTool state', () => {
       assert.equal(component.state('isDraggingTool'), true);
+    });
+
+    it('sets the toolDragStartX state', () => {
+      assert.equal(component.state('toolDragStartX'), 10);
+    });
+
+    it('sets the toolDragStartY state', () => {
+      assert.equal(component.state('toolDragStartY'), 15);
     });
   });
 
@@ -35,12 +43,25 @@ describe('eventHandlers', () => {
 
   describe('Merkaba#handleCanvasDragStop', () => {
     beforeEach(() => {
-      component.setState({ isDraggingTool: true });
+      component.setState({
+        isDraggingTool: true,
+        toolDragStartX: 10,
+        toolDragStartY: 15
+      });
+
       component.instance().handleCanvasDragStop();
     });
 
     it('sets the isDraggingTool state', () => {
       assert.equal(component.state('isDraggingTool'), false);
+    });
+
+    it('sets the toolDragStartX state', () => {
+      assert.equal(String(component.state('toolDragStartX')), 'null');
+    });
+
+    it('sets the toolDragStartY state', () => {
+      assert.equal(String(component.state('toolDragStartY')), 'null');
     });
   });
 });
