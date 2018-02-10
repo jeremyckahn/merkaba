2629d6d9c8e9238b61ca6e06f09afc07071e2b17
Jeremy Kahn <jeremyckahn@gmail.com>
2018-02-10 11:53:53 -0600

    Start standing up shape type system

--- src/enums.js
+++ src/enums.js
@@ -10,3 +10,13 @@ export const selectedToolType = {
   NONE: 'NONE',
   RECTANGLE: 'RECTANGLE'
 };
+
+/**
+ * Values in this enum correspond to standard SVG shape names:
+ * https://www.w3.org/TR/SVG/shapes.html
+ * @property merkaba.module:enums.shapeType
+ * @enum {string}
+ */
+export const shapeType = {
+  RECT: 'rect'
+};
--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@ import './styles/index.sass';
  * @namespace merkaba
  */
 
+/**
+ * @typedef merkaba.svgShape
+ * @type {Object}
+ * @property {merkaba.module:enums.shapeType} type
+ */
+
+/**
+ * @typedef merkaba.svgRect
+ * @see https://www.w3.org/TR/SVG/shapes.html#InterfaceSVGRectElement
+ * @type {merkaba.svgShape}
+ * @property {number} x
+ * @property {number} y
+ * @property {number} width
+ * @property {number} height
+ * @property {number} rx
+ * @property {number} ry
+ */
+
 export { Merkaba } from './components/merkaba';
