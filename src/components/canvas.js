d119fcfe5cbfe7a99c6d748807d896a59789fc3c
Jeremy Kahn <jeremyckahn@gmail.com>
2018-02-10 12:45:19 -0600

    Create Rect helper component

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -3,6 +3,28 @@ import { DraggableCore } from 'react-draggable';
 import { selectedToolType } from '../enums';
 
 /**
+ * @param {number} x
+ * @param {number} y
+ * @param {number} dx
+ * @param {number} dy
+ * @param {string} stroke
+ * @param {string} fill
+ * @param {number} strokeWidth
+ */
+const Rect = ({
+  x, y, dx, dy, stroke, fill, strokeWidth
+}) =>
+  <rect
+    x={x + Math.min(dx, 0)}
+    y={y + Math.min(dy, 0)}
+    width={Math.abs(dx)}
+    height={Math.abs(dy)}
+    stroke={stroke}
+    fill={fill}
+    strokeWidth={strokeWidth}
+  />
+
+/**
  * @class merkaba.Canvas
  * @param {external:Draggable.DraggableEventHandler} handleCanvasDragStart
  * @param {external:Draggable.DraggableEventHandler} handleCanvasDrag
@@ -35,14 +57,14 @@ export const Canvas = ({
       <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
         {isDraggingTool ? (() =>
           selectedTool === selectedToolType.RECTANGLE ?
-            <rect
-              x={toolDragStartX + Math.min(toolDragDeltaX, 0)}
-              y={toolDragStartY + Math.min(toolDragDeltaY, 0)}
-              width={Math.abs(toolDragDeltaX)}
-              height={Math.abs(toolDragDeltaY)}
-              stroke="red"
-              fill="red"
-              strokeWidth="1"
+            <Rect
+              x={toolDragStartX}
+              y={toolDragStartY}
+              dx={toolDragDeltaX}
+              dy={toolDragDeltaY}
+              stroke={'red'}
+              fill={'red'}
+              strokeWidth={1}
             />
             : null
         )() : null}
