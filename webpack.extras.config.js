0db681ce8d633bb7f1254b95a3a12d3f02ef3a4d
Jeremy Kahn <jeremyckahn@gmail.com>
2018-03-03 12:30:38 -0600

    Add build process for demo

---
+++ webpack.extras.config.js
@@ -0,0 +1,32 @@
+const commonConfig = require('./webpack.common.config');
+const path = require('path');
+const Webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const { name, version } = require('./package.json');
+
+const dist = 'dist';
+
+module.exports = Object.assign(commonConfig, {
+  entry: {
+    demo: './demo/index.js'
+  },
+  output: {
+    path: path.join(__dirname, `${dist}`),
+    filename: '[name].js',
+  },
+  plugins: [
+    new Webpack.optimize.UglifyJsPlugin({
+      compress: {
+        dead_code: true,
+        unused: true
+      },
+      output: {
+        comments: false
+      }
+    }),
+    new CopyWebpackPlugin([
+      { from: 'index.html' }
+    ])
+  ]
+});
